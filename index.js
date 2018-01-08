725ca06f8f54f3cebcd071763a7d47bb6a9789fd
sharafian <ben@sharafian.me>
2018-01-07 18:39:03 -0800

    feat: entry point

---
+++ index.js
@@ -0,0 +1,11 @@
+const Client = require('./src/client')
+const KoaCog = require('./src/koa-cog')
+const CogListener = require('./src/listener')
+const CogAccountant = require('./src/cog-accountant')
+
+module.exports = {
+  Client,
+  KoaCog,
+  CogListener,
+  CogAccountant
+}

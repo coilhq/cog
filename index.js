2334d9ca5074de0a7ed030c922804064a8f20125
sharafian <ben@sharafian.me>
2018-05-21 16:35:10 -0700

    feat: restructure package

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const Client = require('./src/client')
 const KoaCog = require('./src/koa-cog')
-const CogListener = require('./src/listener')
 const CogAccountant = require('./src/cog-accountant')
 
-module.exports = {
-  Client,
-  KoaCog,
-  CogListener,
-  CogAccountant
-}
+module.exports = KoaCog
+module.exports.Accountant = CogAccountant
